57d4ce6a51175f4cd1d9207ff893af5c02d93478
Miguel <miguel@jackrabbitsystems.com>
2015-05-10 22:47:03 -0600

    * easy exercises

---
+++ ruby_intro_workbook/easy_questions/quiz_3/exerc_1.rb
@@ -0,0 +1,5 @@
+# Show an easier way to write this array:
+flintstones = ["Fred", "Barney", "Wilma", "Betty", "BamBam", "Pebbles"]
+
+# another way would be:
+flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
---
+++ ruby_intro_workbook/easy_questions/quiz_3/exerc_3.rb
@@ -0,0 +1,7 @@
+# In the previous exercise we added Dino to our array like this:
+flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+# flintstones << "Dino"
+
+# We could have used either Array#concat or Array#push to add Dino to the family.
+# How can we add multiple items to our array? (Dino and Hoppy)
+flintstones.push("Dino").push("Hoppy")
